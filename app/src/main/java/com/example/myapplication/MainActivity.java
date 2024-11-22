16d2cef03364110ed3949b8946b91852d7dae56c
Mufli-Mohideen <mufli2mail@gmail.com>
2024-11-22 14:38:32 +0530

    Meditation field completed

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calorie);
+        setContentView(R.layout.activity_water_intake);
     }
 }
